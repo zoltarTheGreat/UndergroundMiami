c2be13a8f5df9daea32d2edc6f8d2e1231b0cb9b
zoltarTheGreat <cristianccabrera@gmail.com>
2017-09-30 11:45:39 -0400

    Begginig parsing for photos in DB

--- shows/index.php
+++ shows/index.php
@@ -1,6 +1,22 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/modules/navbar.php'); ?>
 <div id="main" class="container">
+<?php
+	try {
+		$stmt = $conn->prepare("SELECT previewimage FROM shows");
+		$stmt->execute();
+
+		// set the resulting array to associative
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+		foreach(new RecursiveIteratorIterator(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+				echo '<div class="venues rounded float-left col-12 col-lg-6"><img class="img-fluid" src="/images/shows/' . $v . '" alt=""></div>';
+				
+		}
+	}
+	catch(PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	}
+	?> 	
 	whoa dog show it up
 </div>
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'); ?>
--- venues/index.php
+++ venues/index.php
@@ -2,5 +2,21 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/modules/navbar.php'); ?>
 <div id="main" class="container">
 	aaaaaa hwe the place here
+	<?php
+	try {
+		$stmt = $conn->prepare("SELECT image FROM venues");
+		$stmt->execute();
+
+		// set the resulting array to associative
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+		foreach(new RecursiveIteratorIterator(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+				echo '<div class="venues rounded float-left col-12 col-lg-6"><img class="img-fluid" src="/images/venues/' . $v . '" alt=""></div>';
+				
+		}
+	}
+	catch(PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	}
+	?> 	
 </div>
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'); ?>
