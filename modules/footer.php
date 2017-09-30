91362f9102e2fa0b447538eaa924bce06dce754b
mia_fiu <mia@fiu.edu>
2017-09-30 02:17:17 -0400

    Added base structure using bootstrap cover template and other code scraps

---
+++ modules/footer.php
@@ -0,0 +1,18 @@
+<div class="mastfoot">
+	<div class="inner">
+		<p>Created by <a href="https://github.com/rslay">rslay</a> and <a href="https://github.com/zoltarTheGreat">zoltarTheGreat</a> at <a href="https://shellhacks.net/">#Shellhacks2017</a>.</p>
+	</div>
+</div>
+
+</div>
+
+
+</div>
+
+
+<script src="/js/jquery.min.js"></script>
+<script src="/js/popper.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+</body>
+<!-- </body> -->
+</html>
---
+++ modules/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+    <div class="container">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar10">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <span class="navbar-brand hidden-lg-up">Underground Miami</span>
+        <div class="navbar-collapse collapse" id="navbar10">
+            <ul class="navbar-nav nav-fill w-100 align-items-start">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Venues</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Artists</a>
+                </li>
+                <li class="nav-item hidden-md-down">
+                    <h5 class="my-0"><a class="nav-link" href="/">Underground Miami</a></h5>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Graffiti</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Shows</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<br>
