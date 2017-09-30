6fd2e846cb431b1281edf33b044d07225137ee92
rslay <rhern283@fiu.edu>
2017-09-30 09:18:20 -0400

    .enviroment.php creation

---
+++ .enviroment.php
@@ -0,0 +1,9 @@
+<?php
+$DATABASE_HOSTNAME_ADDRESS="homestead.app"; // Name of the hostname address for the database server
+$DATABASE_PORT="3306";
+
+$DATABASE_NAME="undermiami"; // Put the name of the database being used here
+
+$DATABASE_USER="";           // Put your database username here
+$DATABASE_PASSWORD="";       // Put your database password here
+?>
