fb5aaac0d490db56406199325b42e52787a74af0
mia_fiu <mia@fiu.edu>
2017-09-30 02:38:50 -0400

    Added pages

---
+++ graffiti/index.php
@@ -0,0 +1,6 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/modules/navbar.php'); ?>
+<div id="main" class="container">
+	Graphiuuu
+</div>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/modules/footer.php'); ?>
