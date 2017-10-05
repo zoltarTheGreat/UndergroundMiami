a97fcecb3ff503b35ee1602e2231a88218e290ea
zoltarTheGreat <cristianccabrera@gmail.com>
2017-10-05 13:40:37 -0400

    please ignore this

--- environment.php
+++ environment.php
@@ -5,5 +5,5 @@ $DATABASE_PORT="3306";
 $DATABASE_NAME="undermiami";          // Put the name of the database being used here
 $DATABASE_USER="root";           // Put your database username here
 $DATABASE_PASSWORD="";          // Put your database password here
-$_ENV["SPOTIFY_OAUTH"]="BQAIkNdNW9qd7IsgsEpmbF3T8jqLQNzelH39TKCGyiu67jbpKYgcv5HkF3wQcDBKF2fydc2D6lvDi6qhcp2xeFsF3c4UL8yyLK7Y4-j8kZUtjO1RKtKwdqH2IYq3MHIItLLc-EQV";           // Put your spotify OAuth token here
+$_ENV["SPOTIFY_OAUTH"]="BQBsp5SY8-ne38AYim59Nl1N9iLhRSbjC5UFE572D6yHWBKrmaeeHsmg_0TW3UImU-XpHrkB0Qq5fSip9sj6i-fjKIivFEjdVaycuC-CRK2e6JrFz4SuEwu2AEkiQL7y8AeD8fvfD3D0GAg";           // Put your spotify OAuth token here
 ?>
