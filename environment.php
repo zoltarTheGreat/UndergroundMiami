74cf42f433c2665d907808a5d2fa20f1880fdb61
zoltarTheGreat <cristianccabrera@gmail.com>
2017-10-05 13:58:39 -0400

    fix

--- environment.php
+++ environment.php
@@ -3,14 +3,7 @@ $DATABASE_HOSTNAME_ADDRESS="localhost"; // Name of the hostname address for the 
 $DATABASE_PORT="3306";
 
 $DATABASE_NAME="undermiami";          // Put the name of the database being used here
-<<<<<<< HEAD
 $DATABASE_USER="root";           // Put your database username here
 $DATABASE_PASSWORD="";          // Put your database password here
 $_ENV["SPOTIFY_OAUTH"]="BQBsp5SY8-ne38AYim59Nl1N9iLhRSbjC5UFE572D6yHWBKrmaeeHsmg_0TW3UImU-XpHrkB0Qq5fSip9sj6i-fjKIivFEjdVaycuC-CRK2e6JrFz4SuEwu2AEkiQL7y8AeD8fvfD3D0GAg";           // Put your spotify OAuth token here
-=======
-
-$DATABASE_USER="root";           // Put your database username here
-$DATABASE_PASSWORD="";          // Put your database password here
-$_ENV["SPOTIFY_OAUTH"]="BQAIkNdNW9qd7IsgsEpmbF3T8jqLQNzelH39TKCGyiu67jbpKYgcv5HkF3wQcDBKF2fydc2D6lvDi6qhcp2xeFsF3c4UL8yyLK7Y4-j8kZUtjO1RKtKwdqH2IYq3MHIItLLc-EQV";           // Put your spotify OAuth token here
->>>>>>> 5e18bf1d6350c4ff0dbf1173a65c7bca55c3cc41
 ?>
